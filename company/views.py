cb986d18f9774775a5c43683a7e666016403b5f0
byeonghwijeong <byeonghwijeong@gmail.com>
2022-08-17 12:28:59 +0900

    feat	: 채용공고 수정(Update) API 구현
    - [Post] 채용공고 수정 성공시 StatusCode 200
    - URI : ~ /company/job/<int:job_id>/update
    	- job_id는 URI를 통해서 Get
    	- 나머지 Date(incentive, detail ..) Body에서 Get

--- company/views.py
+++ company/views.py
@@ -5,8 +5,9 @@ from django.http import JsonResponse
 
 from .models import Job
 
+
 class PostJobView(View):
-    def post(self,request):
+    def post(self, request):
         try:
             job_data  = json.loads(request.body)
             id        = job_data["id"]
@@ -27,3 +28,26 @@ class PostJobView(View):
 
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+
+class UpdateJobView(View):
+    def post(self, request, job_id):
+        try:
+            job_data  = json.loads(request.body)
+            position  = job_data["position"]
+            incentive = job_data["incentive"]
+            detail    = job_data["detail"]
+            stack     = job_data["stack"]
+
+            Job.objects.filter(id=job_id).update(
+                position   = position,
+                incentive  = incentive,
+                detail     = detail,
+                stack      = stack
+            )
+
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
