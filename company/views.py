b1b9913e50600e8ff87eb954ba37165ea0f0e895
byeonghwijeong <byeonghwijeong@gmail.com>
2022-08-17 15:59:46 +0900

    feat	: 채용공고 상세페이지 API 구현
    - [Get] URI : ~/company/job/<int:job_id>
    - 회사가 올린 다른 채용공고 구현
       - if 문을 활용해서 자기 job.id를 제외 처리

--- company/views.py
+++ company/views.py
@@ -90,3 +90,28 @@ class JobListView(View):
 
         return JsonResponse({'results': job_list}, status = 200)
 
+
+class JobDetailView(View):
+    def get(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+            company_jobs = [
+                company_jobs.id for company_jobs in job.company.job_set.all() \
+                    if not company_jobs.id == job_id
+            ]
+            job_detail = {
+                "id"          : job.id,
+                "company"     : job.company.name,
+                "country"     : job.company.region.country.name,
+                "region"      : job.company.region.name,
+                "position"    : job.position,
+                "incentive"   : job.incentive,
+                "stack"       : job.stack,
+                "detail"      : job.detail,
+                "company_jobs": company_jobs
+            }
+            
+            return JsonResponse({"results": job_detail}, status=200)
+
+        except Job.DoesNotExist:
+            return JsonResponse({"MESSAGE": "NOT_EXIST"}, status=400)
