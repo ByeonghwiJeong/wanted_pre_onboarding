98b75bdc7a719daab6ae6e650f7b4e28d3780b20
byeonghwijeong <byeonghwijeong@gmail.com>
2022-08-17 15:28:43 +0900

    feat	: 채용공고 목록 & 검색 API 구현
    - [Get] URI : ~/company/job?search=검색어
    - ~/compnay/job : 전체목록
    - search=검색어
      - 회사명 & 채용포지션 동시에 검색

--- company/views.py
+++ company/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.views import View
 from django.http import JsonResponse
+from django.db.models import Q
 
 from .models import Job
 
@@ -60,4 +61,53 @@ class UpdateJobView(View):
 
         except Job.DoesNotExist:
             return JsonResponse({"MESSAGE": "NOT_EXIST"}, status=400)
+
+class JobListView(View):
+    def get(self, request):        
+        search = request.GET.get('search')
+        q = Q()
+
+        if search:
+            q &= Q(position__contains = search)
+            q |= Q(company_id__name__contains = search)
+
+        jobs = Job.objects.filter(q)
+
+        job_list = [
+            {
+                "id"       : job.id,
+                "company"  : job.company.name,
+                "country"  : job.company.region.country.name,
+                "region"   : job.company.region.name,
+                "position" : job.position,
+                "incentive": job.incentive,
+                "stack"    : job.stack
+            }
+            for job in jobs
+        ]
+
+        return JsonResponse({'results': job_list}, status = 200)
+
+class JobDetailView(View):
+    def get(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+            company_jobs = [
+                company_jobs.id for company_jobs in job.company.job_set.all()
+            ]
+            job_detail = {
+                "id"          : job.id,
+                "company"     : job.company.name,
+                "country"     : job.company.region.country.name,
+                "region"      : job.company.region.name,
+                "position"    : job.position,
+                "incentive"   : job.incentive,
+                "stack"       : job.stack,
+                "detail"      : job.detail,
+                "company_jobs": company_jobs
+            }
             
+            return JsonResponse({"results": job_detail}, status=200)
+
+        except Job.DoesNotExist:
+            return JsonResponse({"MESSAGE": "NOT_EXIST"}, status=400)
